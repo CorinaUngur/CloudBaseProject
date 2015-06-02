a0f2f20caa623b1cc82d22a4a1203e6632dfbcf8
korrr <ccorina.ungur@gmail.com>
2015-06-02 17:07:37 +0300

    first amqp test

---
+++ controler/receive.py
@@ -0,0 +1,15 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+
+channel.queue_declare(queue="hello")
+
+print 'Waiting...'
+
+def callback(ch, method, properties, body):
+	print 'received: ' + body
+
+channel.basic_consume(callback, queue='hello', no_ack=True)
+
+channel.start_consuming()
