d048b1a422617152673e69d5837ee06bf7a3d220
korrr <ccorina.ungur@gmail.com>
2015-06-02 17:55:41 +0300

    try multiple queues

--- agentTest/send.py
+++ agentTest/send.py
@@ -6,7 +6,20 @@ channel = connection.channel()
 channel.queue_declare(queue="hello")
 
 channel.basic_publish(exchange='', routing_key='hello', body='Hey there')
-
 print '[x] Sent "hey there"'
+
+channel.queue_declare(queue="listener1")
+
+channel.basic_publish(exchange='', routing_key='listener1', body='Hey listener1, nice to meet you')
+print '[x] Sent message for listener1'
+
+
+channel.queue_declare(queue="listener2")
+
+channel.basic_publish(exchange='', routing_key='listener2', body='Hey listener2, are you ready?')
+print '[x] Sent message for listener2'
+
+
+
 
 connection.close()
--- controler/receive.py
+++ controler/receive.py
@@ -10,6 +10,30 @@ print 'Waiting...'
 def callback(ch, method, properties, body):
 	print 'received: ' + body
 
+channel.basic_qos(prefetch_count=1)
+
 channel.basic_consume(callback, queue='hello', no_ack=True)
+
+
+
+channel.queue_declare(queue="listener1")
+
+print 'listener 1 channel declared queue'
+
+def callback2(ch, method, properties, body):
+	print 'listener1 received: ' + body
+
+channel.basic_consume(callback2, queue='listener1', no_ack=True)
+
+
+
+print 'listener 2 channel declared queue'
+
+def callback3(ch, method, properties, body):
+	print 'listener2 received: ' + body
+
+channel.basic_consume(callback3, queue='listener2', no_ack=True)
+
+
 
 channel.start_consuming()
