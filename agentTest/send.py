e334cf692c3134052f1e43cd84a19bb29af5071d
korrr <ccorina.ungur@gmail.com>
2015-06-03 01:27:53 +0300

    created first agent class

--- agentTest/send.py
+++ agentTest/send.py
@@ -1,25 +1,17 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-channel = connection.channel()
 
-channel.queue_declare(queue="hello")
+class Agent:
+	def __init__(self, hos="localhost"):
+		self.host = hos
+		self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
+		self.channel = self.connection.channel()
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hey there')
-print '[x] Sent "hey there"'
+	def send_message(self, que, message):
+		self.channel.queue_declare(queue=que)
+		self.channel.basic_publish(exchange='', routing_key=que, body=message)
+		print '# Sent to '+que+': '+message
 
-channel.queue_declare(queue="listener1")
+	def close_connection(self):
+		self.connection.close()
 
-channel.basic_publish(exchange='', routing_key='listener1', body='Hey listener1, nice to meet you')
-print '[x] Sent message for listener1'
-
-
-channel.queue_declare(queue="listener2")
-
-channel.basic_publish(exchange='', routing_key='listener2', body='Hey listener2, are you ready?')
-print '[x] Sent message for listener2'
-
-
-
-
-connection.close()
